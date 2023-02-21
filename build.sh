47cef166e79939e7f8898c328481bfbf9cfb8898
Konstantinos Parasyris <parasyris1@llnl.gov>
2023-02-21 07:51:19 -0800

    Rename all symbols containing '.' in their name into '_'

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+#
+host=$(hostname)
+host=${host//[0-9]/}
+
+mkdir -p build/${host}
+echo $host
+cd build/${host}
+
 cmake \
-    -DLT_LLVM_INSTALL_DIR=/p/vast1/ggeorgak/projects/compilers/record-and-replay/jd/build@tioga/install \
+    -DLT_LLVM_INSTALL_DIR=/usr/WS2/koparasy/PARCO/llvm/qmcpack/install/${host}/ \
     -DCMAKE_C_COMPILER=gcc \
     -DCMAKE_CXX_COMPILER=g++ \
-    ..
+    ../../
+
+make
+cd -
